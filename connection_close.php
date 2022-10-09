0f9aeb4837ea456b4c200f7617e55b1b3a003c4d
bussakornPal <bussakorn.pal@ku.th>
2022-10-09 19:25:06 +0700

    connect db

---
+++ connection_close.php
@@ -0,0 +1,11 @@
+<?php
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "dbehr";
+
+
+    $conn = mysqli_connect($servername,$username,$password);
+    $conn->close();
+?>
