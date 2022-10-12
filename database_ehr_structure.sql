4ee1d9caab60e6f89317129e1fdeb047517475ff
CHUSANA <91516445+CHUSANA@users.noreply.github.com>
2022-10-13 01:23:59 +0700

    Update database_ehr_structure.sql

--- database_ehr_structure.sql
+++ database_ehr_structure.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 12, 2022 at 08:19 PM
+-- Generation Time: Oct 12, 2022 at 08:23 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 7.4.30
 
@@ -70,4 +70,3 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-s
