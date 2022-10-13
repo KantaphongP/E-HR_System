dec307a943978cc5204e3804c007286687e39fdc
KantaphongP <kantaphong.p@ku.th>
2022-10-13 23:31:44 +0700

    Update timesheet_controller.php

--- controllers/timesheet_controller.php
+++ controllers/timesheet_controller.php
@@ -1,16 +1,12 @@
 <?php
     class TimesheetController
     {
-        public function mytimesheet(){
-            require_once('./views/timesheet/timesheet.php'); 
-        }
         public function timesheet()
         {   
             
             $log_list = log::getAll();
             echo "eye";
-            require_once('./views/timesheet/timesheet.php'); 
-        }
+            require_once('./views/timesheet/timesheet.php'); }
         
     }
 ?>
