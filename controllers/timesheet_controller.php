151eb9dd878093e961c3d991f83c07da3328ae0a
KantaphongP <kantaphong.p@ku.th>
2022-10-13 23:00:46 +0700

    3

--- controllers/timesheet_controller.php
+++ controllers/timesheet_controller.php
@@ -1,12 +1,16 @@
 <?php
     class TimesheetController
     {
+        public function mytimesheet(){
+            require_once('./views/timesheet/timesheet.php'); 
+        }
         public function timesheet()
         {   
             
             $log_list = log::getAll();
             echo "eye";
-            require_once('./views/timesheet/timesheet.php'); }
+            require_once('./views/timesheet/timesheet.php'); 
+        }
         
     }
 ?>
